2c9073e1459f81b1454c8666cf34df68a05809a9
smoke-y <SnowSneeze@protonmail.com>
2025-10-23 17:11:27 +0530

    redesigned frontend

--- server.js
+++ server.js
@@ -42,3 +42,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log("Server is running @ %d...", PORT);
 });
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server running on port 8080');
+});
